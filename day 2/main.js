c40d34f05637072b360801a8c2522afe297db3ce
Yash <yashrawat2002.yr@gmail.com>
2023-03-20 21:08:05 +0530

    Day 2 added comments

--- day 2/main.js
+++ day 2/main.js
@@ -1,18 +1,21 @@
 const btn = document.querySelector('.hamburger');
 let open = false;
 
+//detect click
 btn.addEventListener('click', () =>{
     if(!open)
     {
         btn.classList.add('open');
+        // delay for rotate animation
         setTimeout(()=> {
-            btn.classList.add('rot')
+            btn.classList.add('rot')  
         },500)
         open = true;
     }
-
+    
     else{
         btn.classList.remove('rot');
+        // delay for rotate animation
         setTimeout(()=> {
             btn.classList.remove('open')
         },500)
